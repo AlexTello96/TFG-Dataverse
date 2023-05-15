e2c7baa07e78ef0838c61932e1d713f19e78eef0
AlexTello96 <133120823+AlexTello96@users.noreply.github.com>
2023-05-15 13:15:50 +0200

    Edition of patient check document type

--- Form.Paciente.js
+++ Form.Paciente.js
@@ -11,3 +11,42 @@ function postalCodeOnChange()
         CRMCommon.SetValue("oxi_locality", null);
     }
 }
+
+// Comprueba si es un DNI correcto.
+function validateDNI(dni) {
+    if (dni == null)
+        return false;
+    
+    var dniPattern = /^(\d{8})([A-Z])$/;
+
+    if(!dniPattern.test(dni)){
+        return false;
+    }
+
+    else
+    return true;
+   
+}
+
+function checkFiscalId(executionContext) {
+        CRMCommon.FormContextGlobal = executionContext.getFormContext();
+        var fiscalId = CRMCommon.GetValue("oxi_identificationdocumentnumber");
+        var idType = CRMCommon.GetValue("oxi_identificationdocumenttype");
+
+        var isValid = false;
+
+        if (fiscalId != null) {
+            if (idType == 279320000) {           
+                isValid = validateDNI(fiscalId);        
+        }
+        
+        if (!isValid) {
+            var message = "invalid identifier";
+            CRMCommon.FormContextGlobal.getControl("oxi_identificationdocumentnumber").setNotification(message, "NOT_VALID_FISCALID_FIELD");
+            CRMCommon.FormContextGlobal.ui.setFormNotification(message, "ERROR", "NOT_VALID_FISCALID_FORM");
+        } else {
+            CRMCommon.FormContextGlobal.ui.clearFormNotification("NOT_VALID_FISCALID_FORM");
+            CRMCommon.FormContextGlobal.getControl("oxi_identificationdocumentnumber").clearNotification("NOT_VALID_FISCALID_FIELD");
+        }
+        }
+}
